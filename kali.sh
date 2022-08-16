82a44a4d1b63cb7652f1dc546f7f3921101a5ff5
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-08-16 17:43:46 +0530

    adding proot-distro kali.sh nethunter.sh parrot.sh nanorc bashrc zshrc

---
+++ kali.sh
@@ -0,0 +1,9 @@
+# This is a default distribution plug-in.
+# Do not modify this file as your changes will be overwritten on next update.
+# If you want customize installation, please make a copy.
+DISTRO_NAME="Kali Linux (nethunter)"
+
+TARBALL_URL['aarch64']="http://kali.download/nethunter-images/current/rootfs/kalifs-arm64-minimal.tar.xz"
+TARBALL_SHA256['aarch64']="69ac2200c542b6c1c50adca08d914953271c940e302680f7140ea1ca37af82b5"
+TARBALL_URL['arm']="http://kali.download/nethunter-images/current/rootfs/kalifs-armhf-minimal.tar.xz"
+TARBALL_SHA256['arm']="04053cd60199979a634b6cbf48618dd54947f747a55e3a133a08e1058e4f88ee"
---
+++ nethunter.sh
@@ -0,0 +1,11 @@
+# This is a default distribution plug-in.
+# Do not modify this file as your changes will be overwritten on next update.
+# If you want customize installation, please make a copy.
+DISTRO_NAME="Kali Nethunter"
+
+
+TARBALL_URL['aarch64']="https://kali.download/nethunter-images/current/rootfs/kalifs-arm64-full.tar.xz"
+TARBALL_SHA256['aarch64']="fda4320ed07bcaf901c4bcf8719de80238e0e1d827e8287b95d773077b8e56c5191fa128e1ff84c5329c38de1b6566339d17992e815a08ddd6a17bcb0f60cb9e"
+TARBALL_URL['arm']="http://kali.download/nethunter-images/current/rootfs/kalifs-armhf-minimal.tar.xz"
+TARBALL_SHA256['arm']="04053cd60199979a634b6cbf48618dd54947f747a55e3a133a08e1058e4f88ee"
+
