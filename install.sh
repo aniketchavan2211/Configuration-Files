737d47d6d207d7adaa57593ab4992bd134fbd326
De4dly Be4st <66140480+aniketchavan2211@users.noreply.github.com>
2022-08-20 11:21:03 +0530

    Updated adding neofetch to zshrc

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 alias UC='apt update -y && apt upgrade -y && apt-get update -y && apt-get upgrade -y && apt autoremove && pkg update -y && pkg upgrade -y && pkg autoclean'
 
-## motd
+# motd
 echo -en  "\n Welcome to Termux \n\n Hey Aniket,\n\n System info:  \n\n" > ~/../usr/etc/motd
+
+# adding neofetch to zshrc file
+pkg install neofetch
+echo -en "neofetch" > ~/../usr/etc/zshrc
 
