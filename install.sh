9dcc11da44b64f9614a0385d25f7d89ff402e841
De4dly Be4st <66140480+aniketchavan2211@users.noreply.github.com>
2022-08-20 11:12:09 +0530

    Update (adding alias for updating)

--- install.sh
+++ install.sh
@@ -1,29 +1,3 @@
 #/bin/sh
 
-# Updating and Upgrading Packages
-apt update -y && apt upgrade -y && pkg update -y && pkg upgrade -y && apt-get update -y && apt-get upgrade -y && apt autoremove && pkg autoclean
-
-# Installing requiring packages
-pkg install git zsh proot proot-distro openssh nano wget curl python sqlite neofetch man figlet
-
-# Tmux (multi-terminal on same terminal)
-# install by uncomment or use below command
-# pkg install tmux
-
-# Install Oh-My-Zsh & fonts & Powerline10k
-cd ~/
-
-# Oh-My-Zsh
-wget sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Powerline font
-cd ~/
-git clone https://github.com/powerline/fonts.git --depth=1
-cd fonts
-./install.sh
-cd ..
-rm -rf fonts
-
-# Powerline10k theme
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+alias UC='apt update -y && apt upgrade -y && apt-get update -y && apt-get upgrade -y && apt autoremove && pkg update -y && pkg upgrade -y && pkg autoclean'
