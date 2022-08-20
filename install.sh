48e59446f1cf45dff0fcb42fb0924c275a4606b4
De4dly Be4st <66140480+aniketchavan2211@users.noreply.github.com>
2022-08-20 11:17:25 +0530

    Update install.sh motd

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #/bin/sh
 
 alias UC='apt update -y && apt upgrade -y && apt-get update -y && apt-get upgrade -y && apt autoremove && pkg update -y && pkg upgrade -y && pkg autoclean'
+
+## motd
+echo -en  "\n Welcome to Termux \n\n Hey Aniket,\n\n System info:  \n\n" > ~/../usr/etc/motd
+
