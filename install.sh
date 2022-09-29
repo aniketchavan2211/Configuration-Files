10f070fdc2b7b97c79be24b0406995fc21848286
De4dly Be4st <66140480+aniketchavan2211@users.noreply.github.com>
2022-09-29 14:12:59 +0530

    Update
    motd  adding $PREFIX

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 apt update -y && apt upgrade -y && apt-get update -y && apt-get upgrade -y && apt autoremove && pkg update -y && pkg upgrade -y && pkg autoclean
 
 # motd
-echo -en  "\n Welcome to Termux \n\n Hey Aniket,\n\n System info:  \n\n" > ~/../usr/etc/motd
+echo -en  "\n Welcome to Termux \n\n Hey Aniket,\n\n System info:  \n\n" > $PREFIX/etc/motd
 
 # adding neofetch to zshrc file
 pkg install neofetch
