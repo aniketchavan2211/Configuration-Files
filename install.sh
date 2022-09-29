1ef6585d92d207a7e72f6fe50081d802bb521b73
De4dly Be4st <66140480+aniketchavan2211@users.noreply.github.com>
2022-09-29 14:11:15 +0530

    Updated install.sh
    Debugging, Fixing, Formatting

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #/bin/sh
 
-alias UC='apt update -y && apt upgrade -y && apt-get update -y && apt-get upgrade -y && apt autoremove && pkg update -y && pkg upgrade -y && pkg autoclean'
+apt update -y && apt upgrade -y && apt-get update -y && apt-get upgrade -y && apt autoremove && pkg update -y && pkg upgrade -y && pkg autoclean
 
 # motd
 echo -en  "\n Welcome to Termux \n\n Hey Aniket,\n\n System info:  \n\n" > ~/../usr/etc/motd
 
 # adding neofetch to zshrc file
 pkg install neofetch
-echo -en "neofetch" >> ~/../usr/etc/zshrc
+# echo -en "neofetch" >> $PREFIX/etc/zshrc
 
 # adding neofetch to bash.bashrc file
-echo -en "neofetch" >> ~/../usr/etc/bash.bashrc
+# echo -en "neofetch" >> $PREFIX/etc/bash.bashrc
 
 # nano
 pkg install nano
-cd ~/../usr/share/
-git clone https://github.com/scopatz/nanorc.git $install_path
-echo -en 'include "/data/data/com.termux/files/usr/share/nanorc/*.nanorc" ' > ~/../usr/etc/nanorc
+# cd ~/../usr/share/
+# git clone https://github.com/scopatz/nanorc.git $install_path
+# echo -en 'include "$PREFIX/share/nanorc/*.nanorc" ' > $PREFIX/etc/nanorc
 cd ~
