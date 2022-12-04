9682199bdccc118d6c07f8669f93b9b485c7e750
De4dly Be4st <66140480+aniketchavan2211@users.noreply.github.com>
2022-12-05 00:49:49 +0530

    Updated

--- nethunter.sh
+++ nethunter.sh
@@ -1,3 +1,7 @@
+# Personally made issues
+# Offical Kali Full image
+
+
 # This is a default distribution plug-in.
 # Do not modify this file as your changes will be overwritten on next update.
 # If you want customize installation, please make a copy.
